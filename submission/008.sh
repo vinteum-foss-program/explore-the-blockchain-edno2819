55a56f54abf00323a8a4c20ec14bf9b83e5c26b5
edno <edno2819@hotmail.com>
2024-11-24 10:03:46 -0300

    New 08

--- submission/008.sh
+++ submission/008.sh
@@ -4,4 +4,4 @@
 
 # bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 2
 # PUBLICK_KEY=vin[0].previous.scriptPubKey.asm[2:]
-echo "6b6c3a0f972fbbe7e4913001060a6a9796388afddb10975d7fe516474362d9cc"
+echo "025d524ac7ec6501d018d322334f142c7c11aa24b9cffec03161eca35a1e32a71f67"
