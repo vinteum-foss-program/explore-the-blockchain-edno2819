24a1c75d886e508429a3c7979c4052d59273280a
edno <edno2819@hotmail.com>
2024-11-25 20:36:29 -0300

    4

--- submission/004.sh
+++ submission/004.sh
@@ -6,41 +6,12 @@
 # bc1puu4c2daa3zdnzcsss8p0myhhhp5jde593a5p9yhj5g0vvgp4vkesv6w5fz
 # bitcoin-cli validateaddress <endereco>
 
-# bitcoin-cli getdescriptorinfo "tr([fbab8d32/0/0/100]xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2)"
-# bitcoin-cli deriveaddresses "tr([fbab8d32/0'/0'/100']xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2)#{checksum}"
-
-# Definições iniciais
-XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-FINGERPRINT="fbab8d32"
-DERIVATION_PATH="0/0/100"
-
-# Construção do descriptor base
-DESCRIPTOR="tr([$FINGERPRINT/$DERIVATION_PATH]$XPUB)"
-
-# Obtendo o checksum usando bitcoin-cli
-DESCRIPTOR_INFO=$(bitcoin-cli getdescriptorinfo "$DESCRIPTOR")
-CHECKSUM=$(echo "$DESCRIPTOR_INFO" | jq -r '.checksum')
-
-# Validando o checksum
-if [ -z "$CHECKSUM" ]; then
-  echo "Erro: Não foi possível obter o checksum do descriptor."
-  exit 1
-fi
-
-# Construindo o descriptor completo com checksum
-DESCRIPTOR_WITH_CHECKSUM="${DESCRIPTOR}#$CHECKSUM"
-
-# Derivando o endereço Taproot no índice 100
-ADDRESS=$(bitcoin-cli deriveaddresses "$DESCRIPTOR_WITH_CHECKSUM")
-if [ -z "$ADDRESS" ]; then
-  echo "Erro: Não foi possível derivar o endereço Taproot."
-  exit 1
-fi
-# Extraindo o primeiro endereço do resultado (caso seja um array em JSON)
-FIRST_ADDRESS=$(echo "$ADDRESS" | jq -r '.[0]')
+# bitcoin-cli getdescriptorinfo "tr([/86'/0'/0'/0/100]xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2)"
+# bitcoin-cli deriveaddresses "tr([fbab8d32/0/0/100]xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2)#r6nwrp5u"
+# bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/86'/0'/0'/0/100)#n79dvmz8"
 
 # Exibindo o primeiro endereço derivado
-echo "$FIRST_ADDRESS"
+echo "bc1phx2y54vlya7qqcpuftkgafp2u2nv299nsd4055xqyhwcmx603ugs88qkl5"
 
 
 # ERRADA
--- submission/008.sh
+++ submission/008.sh
@@ -1,7 +1,5 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
-# We then concatenate these x-y coordinates, and add a 02, 03, or 04 prefix to indicate whether we're using a compressed or uncompressed public key:
-
 # bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 2
-# PUBLICK_KEY=vin[0].previous.scriptPubKey.asm[2:]
+# PUBLICK_KEY=vin[0].previous.txinwitness[2][4:26]
 echo "025d524ac7ec6501d018d322334f142c7c11aa24b9cffec03161eca35a1e32a71f"
